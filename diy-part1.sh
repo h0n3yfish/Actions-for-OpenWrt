cae14704bcd18cb091528703c84c8209b872710d
h0n3yfish <giddy_scours_0o@icloud.com>
2023-04-05 11:45:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 echo 'src-git alist https://github.com/h0n3yfish/OpenWrt-Extra-Packages' >>feeds.conf.default
+echo 'src-git dae https://github.com/h0n3yfish/OpenWrt-Extra-Packages' >>feeds.conf.default
